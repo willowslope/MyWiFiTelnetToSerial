cf0e3aefaaf03cf354f060c5f41929ea2f06e5e9
willowslope <willowslope@yahoo.co.jp>
2018-02-25 02:49:39 +0900

    Webに対応。コンパイル通っただけ

--- MyWiFiTelnetToSerial.ino
+++ MyWiFiTelnetToSerial.ino
@@ -1,26 +1,132 @@
-#include <ESP8266WiFi.h>
+// ****************************************************************
+// MyWiFiTelnetToSerial
+//  Ver0.01 2018.2.25
+//  copyright Sakayanagi
+// ****************************************************************
 
-//Sleep Timer
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <EEPROM.h>
+// ****************************************************************
+// * Const, RAM
+// ****************************************************************
+
+//Sleep Timer 30蛻�縺ｫ險ｭ螳�
 #define SLEEP_TIMER (unsigned long)(30*60*1000)
 unsigned long time_old = 0;
 
 //how many clients should be able to telnet to this ESP8266
 #define MAX_SRV_CLIENTS 1
 const char *ssid = "ESP_OBDII";
-const char *password = "";
+const char *pass = "";
 WiFiServer server(35000);
 WiFiClient serverClients[MAX_SRV_CLIENTS];
-void setup() {
-  WiFi.mode(WIFI_AP);
-  WiFi.softAPConfig(IPAddress(192, 168, 0, 10), IPAddress(192, 168, 0, 10), IPAddress(255, 255, 255, 0));
-//  WiFi.softAP(ssid, password);
-  WiFi.softAP(ssid);
-  //start UART and the server
+
+ESP8266WebServer websvr(80);  //web繧ｵ繝ｼ繝舌�ｼ
+
+char myssid[33];
+char mypass[64];
+#define EEPROM_NUM (sizeof(myssid)+sizeof(mypass))
+
+// ****************************************************************
+// * Prototype
+// ****************************************************************
+
+// ****************************************************************
+// * HomePage
+// ****************************************************************
+void handleTopPage() {
+  String html = "\
+<!DOCTYPE html><html><head><title>Setting</title></head><body>\
+<H1>ESP_OBDII Setting</H1>\
+<form name='inputform' action='' method='POST'>\
+<H2>WiFi Setting</H2>\
+SSID:<input type='text' name='ssid' maxlength='32' value='**myssid'><br>\
+PASS:<input type='text' name='pass' maxlength='63' value='**mypass'><br>\
+<br><input type='submit' name='SUBMIT' value='Submit'>\
+</form></body></html>\
+<H2>Connection Status</H2>\
+<H3>AP<dl><dt>ssid</dt><dd>**ssid</dd><dt>pass</dt><dd>**pass</dd></dl>\
+<H3>STA <dl><dt>ssid</dt><dd>**myssid</dd><dt>pass</dt><dd>**mypass</dd><dt>IP</dt><dd>**IP</dd></dl>\
+";
+
+  if (websvr.hasArg("ssid")) strcpy(myssid,websvr.arg("ssid").c_str());
+  if (websvr.hasArg("pass")) strcpy(mypass,websvr.arg("pass").c_str());
+  if (websvr.hasArg("SUBMIT")) {
+    unsigned char data[EEPROM_NUM];
+    unsigned char *pd = data;
+    int i;
+    for (i=0;i<sizeof(myssid); i++)  {*pd = myssid[i]; pd++;}
+    for (i=0;i<sizeof(mypass); i++)  {*pd = mypass[i]; pd++;}
+    for (i = 0; i < EEPROM_NUM; i++) {EEPROM.write(i, data[i]);}
+    EEPROM.commit();
+  }
+
+  html.replace("**ssid",ssid);
+  html.replace("**pass",pass);
+  html.replace("**myssid",myssid);
+  html.replace("**mypass",mypass);
+  if(WiFi.status() == WL_CONNECTED) html.replace("**IP",WiFi.localIP().toString());
+  else html.replace("**IP","Disconnect");
+  websvr.send(200, "text/html", html);
+}
+
+// -----------------------------------
+// Not Found
+//------------------------------------
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += websvr.uri();
+  message += "\nMethod: ";
+  message += (websvr.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += websvr.args();
+  message += "\n";
+  for (uint8_t i = 0; i < websvr.args(); i++) {
+    message += " " + websvr.argName(i) + ": " + websvr.arg(i) + "\n";
+  }
+  websvr.send(404, "text/plain", message);
+}
+// ****************************************************************
+// * setup
+// ****************************************************************
+void setup_eeprom(void) {
+  //EEPROM
+  EEPROM.begin(EEPROM_NUM);
+  unsigned char data[EEPROM_NUM];
+  unsigned char *pd = data;
+  int i;
+  for (i = 0; i < EEPROM_NUM; i++) {data[i] = EEPROM.read(i);}
+  for (i=0;i<sizeof(myssid);i++){myssid[i] = *pd; pd++;}
+  if (strlen(myssid)==0){strcpy(myssid,"myssid");}
+  for (i=0;i<sizeof(mypass);i++){mypass[i] = *pd; pd++;}
+}
+
+void setup_com(void){
   Serial.begin(38400);
+
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.begin(myssid,mypass);
+  
+  WiFi.softAPConfig(IPAddress(192, 168, 0, 10), IPAddress(192, 168, 0, 10), IPAddress(255, 255, 255, 0));
+  WiFi.softAP(ssid, pass);
+
   server.begin();
   server.setNoDelay(true);
+}
 
-    
+// ------------------------------------
+void setup_http(void) {
+  websvr.on("/", handleTopPage);
+  websvr.onNotFound(handleNotFound);
+  websvr.begin();
+}
+
+void setup() {
+  setup_eeprom();
+  setup_com();
+  setup_http();
 }
 void loop() {
   uint8_t i;
@@ -36,8 +142,6 @@ void loop() {
       if (!serverClients[i] || !serverClients[i].connected()){
         if(serverClients[i]) serverClients[i].stop();
         serverClients[i] = server.available();
-//        serverClients[i].println("Telnet Start(WiFi)");
-//        Serial.println("Telnet Start(COM)");
         continue;
       }
     }
@@ -68,5 +172,8 @@ void loop() {
       }
     }
   }
+
+  websvr.handleClient();
 }
+
 
